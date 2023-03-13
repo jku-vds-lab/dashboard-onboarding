158c84820f66008522df1bdd18b60d5baffb0d15
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-03-13 16:32:40 +0100

    code cleanup

--- src/UI/nodes-canvas/canvas-index.jsx
+++ src/UI/nodes-canvas/canvas-index.jsx
@@ -1,16 +1,8 @@
-import { useState, useRef, useCallback, MouseEvent } from "react";
-import ReactFlow, {
-  useNodesState,
-  Controls,
-  useReactFlow,
-  Node,
-} from "reactflow";
-
+import { useState, useRef, useCallback } from "react";
+import ReactFlow, { useNodesState, Controls, useReactFlow } from "reactflow";
 import "reactflow/dist/style.css";
 import "../assets/css/flow.scss";
 
-// Node types
-import BoxNode from "./nodeTypes/boxNode";
 import SimpleNode from "./nodeTypes/simpleNode";
 import GroupNode from "./nodeTypes/groupNode";
 
@@ -21,20 +13,19 @@ import { getVisualInfos } from "../../onboarding/ts/listOfVisuals";
 
 const initialNodes = [];
 
-const nodeTypes = { boxNode: BoxNode, simple: SimpleNode, group: GroupNode };
+const nodeTypes = { simple: SimpleNode, group: GroupNode };
 
 // let id = 0;
 // const getId = () => `dndnode_${id++}`;
 
-function NodesCanvas() {
-  const reactFlowWrapper = useRef(null);
+export default function NodesCanvas() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [nodeData, setNodeData] = useState(null);
-  const { getIntersectingNodes } = useReactFlow();
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const { getIntersectingNodes } = useReactFlow();
+  const [nodeData, setNodeData] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const reactFlowWrapper = useRef(null);
 
   const onClick = useCallback(async (event) => {
     let container = document.getElementById("canvas-container");
@@ -68,7 +59,6 @@ function NodesCanvas() {
 
   const onDrop = useCallback(
     (event) => {
-      // debugger;
       event.preventDefault();
       const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
       const id = event.dataTransfer.getData("id");
@@ -86,7 +76,6 @@ function NodesCanvas() {
         y: event.clientY - reactFlowBounds.top,
       });
 
-      console.log("Old position of ", id, "position", position);
       const newNode = {
         id,
         type,
@@ -101,22 +90,6 @@ function NodesCanvas() {
     },
     [reactFlowInstance, setNodes]
   );
-
-  /**
-   * Called, when the node is clicked – do something
-   * @type {(function(): void)|*}
-   */
-  const onNodeClick = useCallback((event, el) => {
-    /* if(el.selected) {
-                 setNodes((nds) =>
-                     nds.map((n) => {
-                         if (n.id === el.id) {
-                             n.selected = false
-                         }
-                         return n;
-                     }));
-             }*/
-  }, []);
 
   const onNodeDragStart = useCallback((event, node) => {
     const intersections = getIntersectingNodes(node).map((n) => n.id);
@@ -164,14 +137,9 @@ function NodesCanvas() {
           setNodes((nodes) =>
             nodes.map((n) => {
               if (n.id === node.id) {
-                // it's important that you create a new object here
-                // in order to notify react flow about the change
                 n.parentNode = iNode.id;
                 n.position = position;
-
-                console.log("Node position", position.x, position.y);
               }
-              console.log("All positions", n.position);
               return n;
             })
           );
@@ -189,10 +157,6 @@ function NodesCanvas() {
     }
   };
 
-  const onLoad = useCallback(() => {
-    //n-button options
-  }, []);
-
   const onNodeMouseEnter = (e, node) => {
     if (node.type === "group") {
     }
@@ -201,7 +165,6 @@ function NodesCanvas() {
 
   const onNodeContextMenu = useCallback((e) => {
     e.preventDefault();
-    debugger;
     const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
     setPosition({
       x: e.clientX - reactFlowBounds.left,
@@ -212,7 +175,6 @@ function NodesCanvas() {
 
   const deleteNode = () => {
     if (nodeData.type === "group") {
-      //delete children nodes
       setNodes((nodes) => nodes.filter((n) => n.parentNode !== nodeData.id));
       setNodes((nodes) => nodes.filter((n) => n.id !== nodeData.id));
     } else {
@@ -225,20 +187,16 @@ function NodesCanvas() {
     <div className="dndflow">
       <div className="reactflow-wrapper" ref={reactFlowWrapper}>
         <ReactFlow
-          nodes={nodes}
-          // edges={edges}
-          onNodesChange={onNodesChange}
-          nodeTypes={nodeTypes}
           onInit={setReactFlowInstance}
+          nodes={nodes}
+          nodeTypes={nodeTypes}
           onDrop={onDrop}
-          // onNodeDragStart={onNodeDragStart}
-          onNodeDragStop={onNodeDragStop}
-          onDragOver={onDragOver}
-          onNodeClick={onNodeClick}
-          onNodeMouseEnter={onNodeMouseEnter}
           onClick={onClick}
-          onNodeDragStart={onNodeDragStart} // this was onNodeDrag
-          onLoad={onLoad}
+          onDragOver={onDragOver}
+          onNodesChange={onNodesChange}
+          onNodeDragStart={onNodeDragStart}
+          onNodeDragStop={onNodeDragStop}
+          onNodeMouseEnter={onNodeMouseEnter}
           onNodeContextMenu={onNodeContextMenu}
           snapToGrid
           fitView
@@ -255,5 +213,3 @@ function NodesCanvas() {
     </div>
   );
 }
-
-export default NodesCanvas;
