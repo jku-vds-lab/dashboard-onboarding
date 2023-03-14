63a6ac6116fbf2eb88deafc1a66eef6a15f811d2
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-03-14 15:10:39 +0100

    components-pane cleaned up

--- src/UI/input-pane/component-properties.jsx
+++ src/UI/input-pane/component-properties.jsx
@@ -5,24 +5,68 @@ import "../assets/css/dashboard.scss";
 
 export default function ComponentsProperties(className, parentId) {
   for (const vis of allVisuals) {
-    // improve the logic here
     let visTitle = createNodeTitle(vis.type);
-    let index = 0;
     const itemLength = checkDuplicateComponents(vis.type);
     if (itemLength > 1) {
-      index = index + 1;
-      visTitle = visTitle + index;
+      visTitle = visTitle + " (" + vis.title + ")";
     }
 
+    let ids = [];
+    let contents = [];
+    let visName = "";
+    let visClassName = "";
+
+    ids.push(vis.name);
+    contents.push(visTitle);
+    visClassName = className + " " + visTitle;
+    visName = vis.name;
+    debugger;
+
+    if (parentId.includes("sub")) {
+      debugger;
+      let result = getSubComponents(ids, contents, vis.type);
+      ids = result?.ids;
+      contents = result?.contents;
+    }
+    ids.forEach((id, idx) => {
+      createNode(id, visClassName, contents[idx], parentId, visName, visTitle);
+    });
+  }
+
+  function createNode(id, visClassName, content, parentId, visName, visTitle) {
     const div = document.createElement("div");
-    div.id = vis.name;
-    div.className = className + " " + visTitle;
-    div.innerHTML = visTitle;
+    div.id = id;
+    div.className = visClassName;
+    div.innerHTML = content;
     div.setAttribute("draggable", "true");
     div.addEventListener("dragstart", function () {
-      onDragStart(event, "simple", vis.name, visTitle, visTitle);
+      onDragStart(event, "simple", visName, visTitle, visTitle);
     });
     document.getElementById(parentId)?.appendChild(div);
+  }
+
+  function getSubComponents(oldId, oldContent, type) {
+    let ids = [];
+    let contents = [];
+    switch (type) {
+      case "card":
+        ids.push(oldId[0] + " Insight");
+        contents.push(oldContent[0] + " Insight");
+        break;
+      case "slicer":
+        ids.push(oldId[0] + " Interaction");
+        contents.push(oldContent[0] + " Interaction");
+        break;
+      default:
+        ids.push(oldId[0] + " Insight");
+        contents.push(oldContent[0] + " Insight");
+        ids.push(oldId[0] + " Interaction");
+        contents.push(oldContent[0] + " Interaction");
+    }
+    return {
+      ids: ids,
+      contents: contents,
+    };
   }
 
   function checkDuplicateComponents(visType) {
--- src/UI/input-pane/subcomponents-pane.jsx
+++ src/UI/input-pane/subcomponents-pane.jsx
@@ -1,112 +1,12 @@
 import React, { useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import "../assets/css/dashboard.scss";
-import { allVisuals } from "../../onboarding/ts/globalVariables";
+import ComponentsProperties from "./component-properties";
 
 export default function SubcomponentsPane() {
   useEffect(() => {
-    const cardElements = allVisuals.filter(function (visual) {
-      return visual.type == "card";
-    });
-    const cardElementsLength = cardElements.length;
-    let index = 0;
-    for (const vis of allVisuals) {
-      let visTitle = createNodeTitle(vis.type);
-      if (vis.type == "card") {
-        if (cardElementsLength > 1) {
-          index = index + 1;
-          visTitle = visTitle + index;
-        }
-      }
-      switch (vis.type) {
-        case "card":
-          createComponentNode({
-            id: vis.name + " insight",
-            classes: `dndnode`,
-            content: `${visTitle} insight`,
-            function: "",
-            parentId: "subcomponentNodes",
-          });
-          break;
-        case "slicer":
-          createComponentNode({
-            id: vis.name + " interaction",
-            classes: `dndnode`,
-            content: `${visTitle} interaction`,
-            function: "",
-            parentId: "subcomponentNodes",
-          });
-          break;
-        default:
-          createComponentNode({
-            id: vis.name + " interaction",
-            classes: `dndnode`,
-            content: `${visTitle} interaction`,
-            function: "",
-            parentId: "subcomponentNodes",
-          });
-          createComponentNode({
-            id: vis.name + " insight",
-            classes: `dndnode`,
-            content: `${visTitle} insight`,
-            function: "",
-            parentId: "subcomponentNodes",
-          });
-      }
-    }
+    ComponentsProperties("dndnode", "subcomponentNodes");
   }, []);
-
-  function createComponentNode(attributes) {
-    const div = document.createElement("div");
-    div.id = attributes.id;
-    div.className = attributes.classes + " " + attributes.content;
-    div.innerHTML = attributes.content;
-    div.setAttribute("draggable", "true");
-    div.addEventListener("dragstart", function () {
-      onDragStart(
-        event,
-        "simple",
-        attributes.id,
-        attributes.content,
-        attributes.content
-      );
-    });
-
-    document.getElementById(attributes.parentId)?.appendChild(div);
-  }
-
-  function createNodeTitle(title, index = "") {
-    let newTitle = title;
-    switch (title) {
-      case "card":
-        newTitle = "KPI";
-        break;
-      case "slicer":
-        newTitle = "Filter";
-        break;
-      case "lineClusteredColumnComboChart":
-        newTitle = "Column";
-        break;
-      case "clusteredBarChart":
-        newTitle = "Bar";
-        break;
-      case "lineChart":
-        newTitle = "Line";
-        break;
-      default:
-        newTitle = title;
-    }
-    newTitle = newTitle + index;
-    return newTitle;
-  }
-
-  const onDragStart = (event, nodeType, nodeId, nodeData, title) => {
-    event.dataTransfer.setData("nodeType", nodeType);
-    event.dataTransfer.setData("id", nodeId);
-    event.dataTransfer.setData("data", nodeData);
-    event.dataTransfer.setData("title", title);
-    event.dataTransfer.effectAllowed = "move";
-  };
 
   return (
     <Accordion.Item eventKey="2">
