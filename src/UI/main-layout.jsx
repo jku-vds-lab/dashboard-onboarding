d22ca9f105325b2e32fc9f851081f200ab06f68c
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-03-14 10:43:24 +0100

    merged with master changes

--- src/UI/main-layout.jsx
+++ src/UI/main-layout.jsx
@@ -1,12 +1,9 @@
 import "./assets/css/dashboard.scss";
-import { setDivisor } from "../onboarding/ts/sizes";
 import InputPane from "./input-pane/main-input-pane";
 import StoryPane from "./story-pane/main-story-pane";
 import OutputPane from "./output-pane/main-output-pane";
 
 export default function MainLayout() {
-  setDivisor(3);
-
   return (
     <div
       className="d-board"
