9fe5c271119156212b3d149d30491dfb7f31cdd3
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-03-14 10:28:18 +0100

    remove commented lines

--- src/UI/input-pane/components-pane.jsx
+++ src/UI/input-pane/components-pane.jsx
@@ -1,4 +1,3 @@
-import { allVisuals } from "../../onboarding/ts/globalVariables";
 import React, { useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import "../assets/css/dashboard.scss";
@@ -7,74 +6,7 @@ import ComponentsProperties from "./component-properties";
 export default function ComponentsPane() {
   useEffect(() => {
     ComponentsProperties("dndnode", "componentNodes");
-    // for (const vis of allVisuals) {
-    //   let index = 0;
-    //   let visTitle = createNodeTitle(vis.type);
-    //   const itemLength = checkDuplicateComponents(vis.type);
-    //   if (itemLength > 1) {
-    //     index = index + 1;
-    //     visTitle = visTitle + index;
-    //   }
-
-    //   createComponentNode({
-    //     id: vis.name,
-    //     classes: `dndnode`,
-    //     content: `${visTitle}`,
-    //     parentId: "componentNodes",
-    //   });
-    // }
   }, []);
-
-  // function createComponentNode(attributes) {
-  //   const div = document.createElement("div");
-  //   div.id = attributes.id;
-  //   div.className = attributes.classes + " " + attributes.content;
-  //   div.innerHTML = attributes.content;
-  //   div.setAttribute("draggable", "true");
-  //   div.addEventListener("dragstart", function () {
-  //     onDragStart(
-  //       event,
-  //       "simple",
-  //       attributes.id,
-  //       attributes.content,
-  //       attributes.content
-  //     );
-  //   });
-
-  //   document.getElementById(attributes.parentId)?.appendChild(div);
-  // }
-
-  // const checkDuplicateComponents = (visType) => {
-  //   const componentItems = allVisuals.filter(function (visual) {
-  //     return visual.type == visType;
-  //   });
-  //   return componentItems.length;
-  // };
-
-  // function createNodeTitle(title, index = "") {
-  //   let newTitle = title;
-  //   switch (title) {
-  //     case "card":
-  //       newTitle = "KPI";
-  //       break;
-  //     case "slicer":
-  //       newTitle = "Filter";
-  //       break;
-  //     case "lineClusteredColumnComboChart":
-  //       newTitle = "Column";
-  //       break;
-  //     case "clusteredBarChart":
-  //       newTitle = "Bar";
-  //       break;
-  //     case "lineChart":
-  //       newTitle = "Line";
-  //       break;
-  //     default:
-  //       newTitle = title;
-  //   }
-  //   newTitle = newTitle + index;
-  //   return newTitle;
-  // }
 
   const onDragStart = (event, nodeType, nodeId, nodeData, title) => {
     event.dataTransfer.setData("nodeType", nodeType);
