5a3bfb03ed93165f61d92ebc4a098c8255914a03
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-04-11 17:42:22 +0200

    fix position and update rank and prob

--- src/UI/nodes-canvas/traversal.jsx
+++ src/UI/nodes-canvas/traversal.jsx
@@ -82,7 +82,6 @@ class TraversalOrder {
         }
       }
     });
-    // next level of complication will be the position
   }
 
   setRank() {
@@ -109,6 +108,7 @@ class TraversalOrder {
       let yPos2 = elem2.positionAbsolute
         ? elem2.positionAbsolute.y
         : elem2.position.y;
+
       if (yPos1 < yPos2) {
         return -1;
       } else if (yPos1 > yPos2) {
@@ -120,6 +120,33 @@ class TraversalOrder {
       console.log("Error", error);
     }
     return 0;
+  }
+
+  updateRank() {
+    this.simpleNodes.forEach((sNode) => {
+      if (sNode.pGrp) {
+        if (sNode.pGrpCat == this.grpCat1) {
+          sNode.probability = 1;
+        } else {
+          sNode.probability = 1 / sNode.pGrpCnt;
+        }
+      }
+    });
+  }
+
+  updateProb() {
+    // Get the information from what was clicked by the user
+    // based on that, set the probabilities of others accordingly
+    // old nodes can simply be discarded
+    this.simpleNodes.forEach((sNode) => {
+      if (sNode.pGrp) {
+        if (sNode.pGrpCat == this.grpCat1) {
+          sNode.probability = 1;
+        } else {
+          sNode.probability = 1 / sNode.pGrpCnt;
+        }
+      }
+    });
   }
 
   onClick(props) {
