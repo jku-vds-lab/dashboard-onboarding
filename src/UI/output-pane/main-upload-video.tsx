d50ba2600c706ef0db82bb5cf641fb39c47b6a44
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-05-23 14:40:23 +0200

    worked on GDrive

--- src/UI/output-pane/main-upload-video.tsx
+++ src/UI/output-pane/main-upload-video.tsx
@@ -5,7 +5,7 @@ export default function UploadVideo() {
     <div>
       <video controls autoPlay>
         <source
-          src="https://pro2future-my.sharepoint.com/:v:/g/personal/vaishali_dhanoa_pro2future_at/EePprVnwemtKsqcSQDbjG5wBm9-To0CJwUs1ybCfR3URJQ?e=WE6kB6"
+          src="https://drive.google.com/uc?export=view&id=1a8ZN5rN5l1Og3ROS4t_X6hu8k8jcga30"
           type="video/mp4"
         />
         Your browser does not support the video tag.
