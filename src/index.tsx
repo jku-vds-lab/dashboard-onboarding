38db2481998e9dc974b219569caf79005d69c23e
VAISHALI-DHANOA <vaishali.dhanoa@pro2future.at>
2023-06-02 11:38:19 +0200

    setting browser router

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/dashboard-onboarding">
     <App />
   </BrowserRouter>
 );
