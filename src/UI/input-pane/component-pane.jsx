14d8a3b2f817ca8d30279e039e490903633b74c1
Ihavenoimagination <e1327973@student.tuwien.ac.at>
2023-07-10 10:43:37 +0200

    update component graph structure

--- src/UI/input-pane/component-pane.jsx
+++ src/UI/input-pane/component-pane.jsx
@@ -1,9 +1,7 @@
 import Components from "./component-properties";
-import { Accordion } from "react-bootstrap";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Nav from 'react-bootstrap/Nav';
-import Tab from 'react-bootstrap/Tab';
+import React, { useState, useEffect } from 'react';
+import { PhotoshopPicker } from 'react-color';
+import { Nav, Tab, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import "../assets/css/dashboard.scss";
 import icon from "../nodes-canvas/icon-1.svg";
@@ -15,11 +13,13 @@ import c_icon_5 from "../assets/img/icon-2.png";
 import c_icon_6 from "../assets/img/icon-4.png";
 import { allVisuals } from "../../onboarding/ts/globalVariables";
 
-export default function ComponentPane() {
-    const tabsData = [];
 
-    let tab =  { eventKey: 'dashboard', tooltip: 'Dashboard', iconSrc: c_icon, className: 'dashboard', headerText: 'Dashboard' };
-    tabsData.push(tab);
+export default function ComponentPane() {
+    const [tabsData, setTabsData] = useState([{ eventKey: 'dashboard', tooltip: 'Dashboard', iconSrc: c_icon, className: 'dashboard', headerText: 'Dashboard',  colorVariable: '--dashboard-color', colorValue: '#4e91e9' },
+        { eventKey: 'globalFilters', tooltip: 'Global Filters', iconSrc: c_icon_2, className: 'globalFilters', headerText: 'Global Filters', colorVariable: '--filters-color', colorValue: '#e7298a' }]);
+
+    /*let tab =  { eventKey: 'dashboard', tooltip: 'Dashboard', iconSrc: c_icon, className: 'dashboard', headerText: 'Dashboard',  colorVariable: '--dashboard-color', colorValue: '#4e91e9' };
+    tabsData.push(tab);*/
     
     for(let i = 0; i < allVisuals.length; i++){
         const tab = {};
@@ -29,12 +29,65 @@ export default function ComponentPane() {
         tab.iconSrc = visData[2];
         tab.className = visData[1];
         tab.headerText = visData[0] + " " + allVisuals[i].title;
+        tab.colorVariable = '--'+allVisuals[i].name+'-color';
+        tab.colorValue = '#4e91e9';
         tabsData[i+1] = tab;
     }
 
-    tab = { eventKey: 'globalFilters', tooltip: 'Global Filters', iconSrc: c_icon_2, className: 'globalFilters', headerText: 'Global Filters' };
-    tabsData.push(tab);
+    /*let tab = { eventKey: 'globalFilters', tooltip: 'Global Filters', iconSrc: c_icon_2, className: 'globalFilters', headerText: 'Global Filters', colorVariable: '--filters-color', colorValue: '#e7298a' };
+    tabsData.push(tab);*/
 
+    const [selectedTab, setSelectedTab] = useState('dashboard');
+    const [isPickerVisible, setPickerVisible] = useState(false);
+    const [componentColor, setComponentColor] = useState(tabsData.find(tab => tab.eventKey === selectedTab).colorValue);
+
+    useEffect(() => {
+        const root = document.documentElement;
+        tabsData.forEach(tab => {
+            root.style.setProperty(tab.colorVariable, tab.colorValue);
+        });
+    }, []);
+
+
+    useEffect(() => {
+        const root = document.documentElement;
+        const selectedColorVariable = tabsData.find(tab => tab.eventKey === selectedTab).colorVariable;
+        root.style.setProperty(selectedColorVariable, componentColor);
+    }, [selectedTab, componentColor]);
+    const handleClick = (eventKey) => {
+        setSelectedTab(eventKey);
+        const selectedColor = tabsData.find(tab => tab.eventKey === selectedTab).colorValue;
+        setComponentColor(selectedColor);
+        setPickerVisible(true);
+    };
+
+    const handleAccept = (color) => {
+        const selectedColorVariable = tabsData.find(tab => tab.eventKey === selectedTab).colorVariable;
+        console.log(selectedTab);
+        const updatedTabsData = tabsData.map(tab => {
+            if (tab.eventKey === selectedTab) {
+                return {
+                    ...tab,
+                    colorValue: componentColor
+                };
+            }
+            return tab;
+        });
+
+
+        setTabsData(updatedTabsData);
+        root.style.setProperty(selectedColorVariable, componentColor);
+        setPickerVisible(false);
+    };
+    const handleOnChangeComplete = (color) => {
+        setComponentColor(color.hex);
+    };
+
+    // Function to handle the cancel action
+    const handleCancel = () => {
+        // Perform necessary actions on cancel
+        setPickerVisible(false); // Hide the PhotoshopPicker
+    };
     function getVisData(visual) {
         let icon = "";
         let type = "";
@@ -99,18 +152,20 @@ export default function ComponentPane() {
         )
     }
 
-    function TabPaneItem({ eventKey, className, headerText }) {
+    function TabPaneItem({ eventKey, className, headerText, colorValue}) {
         return (
             <Tab.Pane eventKey={eventKey} >
-                <div className="tab-body-header"><span>{headerText}</span><span className="color-box"></span></div>
+                <div className="tab-body-header"><span>{headerText}</span>
+                    <span className="color-box" style={{ backgroundColor: colorValue}} onClick={() => handleClick(eventKey)}></span>
+                </div>
                 <Components visual={eventKey}/>
             </Tab.Pane>
         )
     }
   return (
-      <div>
+      <div className="h-100">
           <div className="label">Components</div>
-          <Tab.Container id="component-graph" defaultActiveKey="first">
+          <Tab.Container id="component-graph" defaultActiveKey={tabsData[0].eventKey}>
               <div className="tabs-container">
                   <Nav variant="pills" className="flex-column icon-nav">
                       {tabsData.map(tab =>
@@ -129,13 +184,24 @@ export default function ComponentPane() {
                               eventKey={tab.eventKey}
                               className={tab.className}
                               headerText={tab.headerText}
+                              colorValue = {tab.colorValue}
                           />
                         </div>
                       )}
                   </Tab.Content>
+
               </div>
           </Tab.Container>
-
+          <div className="photoshoppicker">
+              {isPickerVisible && (
+                  <PhotoshopPicker
+                      color={componentColor}
+                      onChangeComplete={handleOnChangeComplete}
+                      onCancel={handleCancel}
+                      onAccept={(color) => handleAccept(color)}
+                  />
+              )}
+          </div>
       </div>
   );
 }
